c1024e01017d121d2a2e3851a6b5047155357355
hangu26 <125523867+hangu26@users.noreply.github.com>
2025-07-05 22:01:34 +0900

    topBar update in MainScreen(Jetpack Compose)

--- app/src/main/java/kr/baeksuk/mealpicker/ui/screens/mainscreen/MainScreen.kt
+++ app/src/main/java/kr/baeksuk/mealpicker/ui/screens/mainscreen/MainScreen.kt
@@ -1,21 +1,27 @@
 package kr.baeksuk.mealpicker.ui.screens.mainscreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import kr.baeksuk.mealpicker.navigation.BottomNavItem
+import kr.baeksuk.mealpicker.R
+import kr.baeksuk.mealpicker.navigation.BottomNavigation
 import kr.baeksuk.mealpicker.navigation.NavigationGraph
 import kr.baeksuk.mealpicker.ui.theme.MealPickerTheme
+import kr.baeksuk.mealpicker.ui.theme.pretendard
+import kr.baeksuk.mealpicker.util.util.pxToDpFixedDpi
+import kr.baeksuk.mealpicker.util.util.pxToSpFixedDpi
 
 @Composable
 fun mainScreen() {
@@ -23,41 +29,36 @@ fun mainScreen() {
     val navController = rememberNavController()
 
     Scaffold(
+        topBar = {
+
+            Column(
+
+                modifier = Modifier
+                    .background(colorResource(id = R.color.white))
+                    .fillMaxSize()
+                    .padding(start = pxToDpFixedDpi(px = 90f), top = pxToDpFixedDpi(px = 60f))
+
+            ) {
+
+                Text(
+                    text = "오늘 뭐 먹어?",
+                    fontSize = pxToSpFixedDpi(px = 80f),
+                    fontFamily = pretendard,
+                    fontWeight = FontWeight.Bold
+                )
+
+            }
+        },
         bottomBar = { BottomNavigation(navController = navController) }
     ) {
-        Box(Modifier.padding(it)){
+        Box(
+            Modifier
+                .padding(it)
+        ) {
             NavigationGraph(navController = navController)
         }
     }
 
-}
-
-@Composable
-fun BottomNavigation(navController: NavHostController) {
-    val items = listOf(
-        BottomNavItem.Home,
-        BottomNavItem.Search,
-        BottomNavItem.Favorite,
-        BottomNavItem.Profile
-    )
-    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
-
-    NavigationBar {
-        items.forEach { item ->
-            NavigationBarItem(
-                icon = { Icon(item.icon, contentDescription = item.label) },
-                label = { Text(item.label) },
-                selected = currentRoute == item.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.startDestinationId) { saveState = true }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
-        }
-    }
 }
 
 @Preview(showBackground = true)
